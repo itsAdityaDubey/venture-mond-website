3fba0bb3484de42217e2fcc2922a20fe827bb2bc
srikanthdevabattula <srikanthdevabathula@gmail.com>
2025-11-13 11:52:23 +0530

    studio page 3 sections

--- src/app/studio/page.jsx
+++ src/app/studio/page.jsx
@@ -1,8 +1,19 @@
+import AboutStudio from "./AboutStudio"
+import FourPillars from "./FourPillars"
+import Hero from "./StudioHero"
+import StudioProcess from "./StudioProcess"
+import WhyChoose from "./WhyChoose"
 
 
 const Studio = () => {
   return (
-    <div>Studio</div>
+    <div>
+      <Hero/>
+      <AboutStudio/>
+      <FourPillars/>
+      <StudioProcess/>
+      <WhyChoose/>
+    </div>
   )
 }
 
